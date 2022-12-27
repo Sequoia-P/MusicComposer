c9689a0ce7ba8fd65204fa3007521109184afcd2
Sequoia Pflug <48298639+Sequoia-P@users.noreply.github.com>
2022-12-27 16:30:10 -0700

    Delete dev directory

--- dev/noteSound.py
+++
@@ -1,5 +0,0 @@
-import StdAudio
-
-class noteSound:
-    def __init__(self, note):
-        StdAudio.playFile("../sounds/" + note)
--- dev/winNoteSound.py
+++
@@ -1,6 +0,0 @@
-import winsound
-
-class noteSound:
-    def __init__(self, note):
-        filename = str(note + ".wav")
-        winsound.PlaySound(filename, winsound.SND_FILENAME)
